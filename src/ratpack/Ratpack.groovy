5c0ba3a61d365c0237106a9da6a6f798991dade8
Rob Zienert <rzienert@bloomhealthco.com>
2014-08-02 19:04:31 -0500

    Indents

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -2,11 +2,11 @@ import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-  handlers {
-    get {
-      render groovyTemplate("index.html", title: "My Ratpack App")
+    handlers {
+        get {
+            render groovyTemplate("index.html", title: "My Ratpack App")
+        }
+
+        assets "public"
     }
-        
-    assets "public"
-  }
 }
