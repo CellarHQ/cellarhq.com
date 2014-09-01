e74027875a572b6ce3c35145bb3cf759e3ccc390
Rob Zienert <rzienert@bloomhealthco.com>
2014-09-01 16:26:57 -0500

    Adding database healthcheck

---
+++ src/main/groovy/com/cellarhq/health/DatabaseHealthcheck.groovy
@@ -0,0 +1,28 @@
+package com.cellarhq.health
+
+import com.codahale.metrics.health.HealthCheck
+import com.google.inject.Inject
+import groovy.transform.CompileStatic
+import ratpack.codahale.metrics.NamedHealthCheck
+
+import javax.sql.DataSource
+
+@CompileStatic
+class DatabaseHealthcheck extends NamedHealthCheck {
+
+    private final DataSource dataSource
+
+    final String name = 'database'
+
+    @Inject
+    DatabaseHealthcheck(DataSource dataSource) {
+        this.dataSource = dataSource
+    }
+
+    @Override
+    protected HealthCheck.Result check() throws Exception {
+        return dataSource.connection.isValid(0) ?
+                HealthCheck.Result.healthy() :
+                HealthCheck.Result.unhealthy('Database connection is invalid')
+    }
+}
