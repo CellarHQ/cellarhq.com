35f009ae5c51c85bfe76fe123a26b9434a9fed4f
Rob Zienert <rzienert@bloomhealthco.com>
2014-10-12 14:47:45 -0500

    Fixing pagination: Now works with other query params

--- web/src/main/java/com/cellarhq/handlebars/helpers/PaginationHelper.java
+++ web/src/main/java/com/cellarhq/handlebars/helpers/PaginationHelper.java
@@ -7,19 +7,21 @@ import java.util.Map;
 import com.github.jknack.handlebars.Options;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import io.netty.handler.codec.http.QueryStringEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ratpack.handlebars.NamedHelper;
+import ratpack.http.Request;
 
-public class PaginationHelper implements NamedHelper<Object> {
+public class PaginationHelper implements NamedHelper<Request> {
 
     private static final Logger logger = LoggerFactory.getLogger(PaginationHelper.class);
 
     public static final String HELPER_NAME = "pagination";
+    public static final String PAGE_QUERY_PARAM = "page";
 
     @Override
-    public CharSequence apply(Object context, Options options) throws IOException {
-
+    public CharSequence apply(Request context, Options options) throws IOException {
         Map<String, Object> paginationInfoMap;
 
         try {
@@ -27,20 +29,9 @@ public class PaginationHelper implements NamedHelper<Object> {
             int totalPageCount = options.param(1, 1);
             int pageGroupCount = options.param(2, 10);
 
-            int firstPageIdx = (((currentPageNumber - 1) / pageGroupCount)) * pageGroupCount + 1;
-            int lastPageIdx = (((currentPageNumber - 1) / pageGroupCount)) * pageGroupCount + pageGroupCount;
+            PagingStrategy strategy = new BasicPagingStrategy(currentPageNumber, totalPageCount, pageGroupCount);
 
-            int previousIdx = lastPageIdx - pageGroupCount;
-            int nextIdx = lastPageIdx + 1;
-
-            boolean canGoPrevious = firstPageIdx > 1 ? true : false;
-            boolean canGoNext = totalPageCount > lastPageIdx ? true : false;
-
-            int displayedLastPage = totalPageCount < lastPageIdx ? totalPageCount : lastPageIdx;
-
-            paginationInfoMap = this.makePaginationInfoMap(canGoPrevious, canGoNext, currentPageNumber, firstPageIdx, displayedLastPage, previousIdx,
-                    nextIdx);
-
+            paginationInfoMap = makePaginationInfoMap(context, strategy);
         } catch (Exception e) {
             logger.debug(e.getMessage());
             paginationInfoMap = Maps.newHashMap();
@@ -49,30 +40,122 @@ public class PaginationHelper implements NamedHelper<Object> {
         return options.fn(paginationInfoMap);
     }
 
-    private Map<String, Object> makePaginationInfoMap(boolean canGoPrevious, boolean canGoNext, int page, int firstPage, int displayedLastPage,
-                                                      int previousIdx, int nextIdx) {
-
-        Map<String, Object> paginationInfoMap = Maps.newHashMap();
+    public Map<String, Object> makePaginationInfoMap(Request request, PagingStrategy strategy) {
+        Map<String, Object> infoMap = Maps.newHashMap();
         List<Map> pageList = Lists.newArrayList();
 
-        for (int i = firstPage; i <= displayedLastPage; i++) {
+        int displayedLastPage = strategy.getLastDisplayedPageIndex();
+        int currentPage = strategy.getCurrentPageNumber();
+
+        for (int i = strategy.getFirstPageIndex(); i <= displayedLastPage; i++) {
             Map<String, Object> numberMap = Maps.newHashMap();
+
+            numberMap.put("pageUri", getUriWithPage(request, i));
             numberMap.put("page", String.valueOf(i));
-            numberMap.put("isCurrent", (i == page ? true : false));
+            numberMap.put("isCurrent", (i == currentPage));
+
             pageList.add(numberMap);
         }
 
-        paginationInfoMap.put("canGoPrevious", canGoPrevious);
-        paginationInfoMap.put("previousIdx", previousIdx);
-        paginationInfoMap.put("pages", pageList);
-        paginationInfoMap.put("canGoNext", canGoNext);
-        paginationInfoMap.put("nextIdx", nextIdx);
+        infoMap.put("canGoPrevious", canGoPrevious(currentPage));
+        infoMap.put("canGoNext", canGoNext(currentPage, strategy.getTotalPageCount()));
+        infoMap.put("previousUri", getUriWithPage(request, strategy.getPreviousIndex()));
+        infoMap.put("nextUri", getUriWithPage(request, strategy.getNextIndex()));
+        infoMap.put("pages", pageList);
 
-        return paginationInfoMap;
+        return infoMap;
+    }
+
+    public boolean canGoPrevious(int currentPage) {
+        return currentPage > 1;
+    }
+
+    public boolean canGoNext(int currentPage, int totalPages) {
+        return currentPage != totalPages;
+    }
+
+    public QueryStringEncoder makeUriEncoder(Request request) {
+        QueryStringEncoder encoder = new QueryStringEncoder(request.getPath());
+
+        for (Map.Entry<String, List<String>> pairs : request.getQueryParams().getAll().entrySet()) {
+            pairs.getValue().stream().filter(value -> !pairs.getKey().equals(PAGE_QUERY_PARAM)).forEach(value -> {
+                encoder.addParam(pairs.getKey(), value);
+            });
+        }
+
+        return encoder;
+    }
+
+    public String getUriWithPage(Request request, int page) {
+        QueryStringEncoder encoder = makeUriEncoder(request);
+        encoder.addParam(PAGE_QUERY_PARAM, String.valueOf(page));
+        return encoder.toString();
     }
 
     @Override
     public String getName() {
         return HELPER_NAME;
+    }
+
+    public interface PagingStrategy {
+
+        public int getCurrentPageNumber();
+        public int getTotalPageCount();
+        public int getPageGroupCount();
+
+        public int getFirstPageIndex();
+        public int getLastPageIndex();
+        public int getNextIndex();
+        public int getPreviousIndex();
+        public int getLastDisplayedPageIndex();
+    }
+
+    public static class BasicPagingStrategy implements PagingStrategy {
+
+        private final int currentPageNumber;
+        private final int totalPageCount;
+        private final int pageGroupCount;
+
+        public BasicPagingStrategy(int currentPageNumber,
+                                   int totalPageCount,
+                                   int pageGroupCount) {
+
+            this.currentPageNumber = currentPageNumber;
+            this.totalPageCount = totalPageCount;
+            this.pageGroupCount = pageGroupCount;
+        }
+
+        public int getCurrentPageNumber() {
+            return currentPageNumber;
+        }
+
+        public int getTotalPageCount() {
+            return totalPageCount;
+        }
+
+        public int getPageGroupCount() {
+            return pageGroupCount;
+        }
+
+        public int getFirstPageIndex() {
+            return (((currentPageNumber - 1) / pageGroupCount)) * pageGroupCount + 1;
+        }
+
+        public int getLastPageIndex() {
+            return (((currentPageNumber - 1) / pageGroupCount)) * pageGroupCount + pageGroupCount;
+        }
+
+        public int getNextIndex() {
+            return getLastPageIndex() + 1;
+        }
+
+        public int getPreviousIndex() {
+            return getLastPageIndex() - pageGroupCount;
+        }
+
+        public int getLastDisplayedPageIndex() {
+            int lastPageIdx = getLastPageIndex();
+            return totalPageCount < lastPageIdx ? totalPageCount : lastPageIdx;
+        }
     }
 }
