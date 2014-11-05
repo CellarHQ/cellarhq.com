c017f567aeaa5de3740ba6eed4d79a8ca2c77abb
John Engelman <john.r.engelman@gmail.com>
2014-11-05 10:46:30 -0600

    Use phantomjs as selenium driver for functional tests.

--- web/src/test/groovy/GebConfig.groovy
+++ web/src/test/groovy/GebConfig.groovy
@@ -1,1 +1,14 @@
+import org.openqa.selenium.Capabilities
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.phantomjs.PhantomJSDriverService
+import org.openqa.selenium.remote.DesiredCapabilities
+
 reportsDir = 'build/geb-reports'
+driver = {
+    Capabilities caps = DesiredCapabilities.phantomjs()
+    def phantomJsDriver = new PhantomJSDriver(PhantomJSDriverService.createDefaultService(caps), caps)
+    phantomJsDriver.manage().window().setSize(new Dimension(1028, 768))
+
+    return phantomJsDriver
+}
